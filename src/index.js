5c02109524b47fb745706e75cf327a82a5b8aebd
SayantanUpGrad <sayantanrulestheworld@gmail.com>
2021-04-30 12:46:21 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Header from './common/header/Header.js';
 //import App from './App';
 //import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <div>
-      
-    </div>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Header />, document.getElementById('root'));
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
